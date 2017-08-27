578c4555423dd5561a52bfee484b81a5ae63481b
吴辉！Yeah <wuhui@17paipai.cn>
2017-08-28 00:52:43 +0800

    添加公共方法

--- vendor/Engine/Common/RequestFunction.php
+++ vendor/Engine/Common/RequestFunction.php
@@ -115,12 +115,59 @@ class RequestFunction
      * @param string $desc
      * @param int $status
      */
-    public static function AjaxJsonReturn($data = array(), $desc = 'Success Response.', $status = 200)
+    public static function ajaxJsonReturn($data = array(), $desc = 'Success Response.', $status = 200)
     {
         echo json_encode(array(
             'status' => intval($status),
             'desc' => $desc,
             'data' => $data
         ));
+    }
+
+    /**
+     * @desc 服务器是否为linux系统
+     */
+    public static function isLinuxSystemOfServer()
+    {
+        return array(
+            'status' => strstr(PHP_OS, 'WIN') ? true : false,
+            'details' => PHP_OS
+        );
+    }
+
+    /**
+     * @desc 检查客户端是否为 iphone
+     */
+    public static function isIphoneDevice()
+    {
+        $is_phone = false;
+
+        if (preg_match_all('/iphone; ios ([0-9._]*)/i', $_SERVER['HTTP_USER_AGENT'], $match)) {
+            $is_phone = true;
+        } else {
+            preg_match_all('/iphone; ios ([0-9._]*)/i', $_SERVER['HTTP_USER_AGENT'], $match);
+        }
+
+        return array(
+            'status' => $is_phone,
+            'version' => $match['0']['0'],
+            'details' => $match['0']['1']
+        );
+    }
+
+    /**
+     * @desc 获取客户端的系统类型
+     */
+    public static function getRemoteEquipmentType()
+    {
+
+    }
+
+    /**
+     * @desc 获取代理服务器地址
+     */
+    public static function getProxyIpAddress()
+    {
+
     }
 }
